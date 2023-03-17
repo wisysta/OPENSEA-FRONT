99e69ed87abe848c88897597bf08d88b1e128a54
wisysta <choi199042@gmail.com>
2023-03-17 18:13:34 +0900

    지갑 컨텍스트 작성

--- context/WalletContext.tsx
+++ context/WalletContext.tsx
@@ -58,6 +58,7 @@ export const WalletContextProvider = ({ children }: PropsWithChildren) => {
 
         // 엑세스 토큰 만료, 리프레시 토큰 발급 및 저장 로직은 이번코딩에선 생략
         setAccessToken(authResult.data.accessToken);
+        setAccount(account);
     }, []);
 
     return (
